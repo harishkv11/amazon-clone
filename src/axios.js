f618157ed851b58c9475b1f24a774c2caa6184db
Harish <harish.kverma1199@gmail.com>
2023-07-26 23:18:54 +0530

    Change Base URL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:5001/clone-v1-78728/us-central1/api'
+    baseURL: 'https://us-central1-clone-v1-78728.cloudfunctions.net/api'
+    // baseURL: 'http://127.0.0.1:5001/clone-v1-78728/us-central1/api'
 })
 
 export default instance;
